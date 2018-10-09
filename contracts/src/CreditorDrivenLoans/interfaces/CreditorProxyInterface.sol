0061f11abddc1f732ba24e2a41a67157ab1b2138
Graeme <graemelearn@protonmail.com>
2018-10-09 14:38:04 -0700

    feat: introduce CreditorProxyInterface

---
+++ contracts/src/CreditorDrivenLoans/interfaces/CreditorProxyInterface.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.25;
+
+import "./CreditorProxyCoreInterface.sol";
+import "./LTVCreditorProxyInterface.sol";
+
+
+contract LTVCreditorProxyInterface is
+	CreditorProxyCoreInterface,
+	LTVCreditorProxyInterface
+{}
