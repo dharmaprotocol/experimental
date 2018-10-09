0a862dc824f887593181cd72299a3aebd16c4833
Graeme <graemelearn@protonmail.com>
2018-10-09 11:30:34 -0700

    feat: implement ltv evaluation

--- contracts/src/CreditorDrivenLoans/DecisionEngines/LTVDecisionEngine.sol
+++ contracts/src/CreditorDrivenLoans/DecisionEngines/LTVDecisionEngine.sol
@@ -1,9 +1,17 @@
 pragma solidity 0.4.24;
 
+// External dependencies
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+
+// Libraries
 import "./libraries/LTVDecisionEngineTypes.sol";
 
 
 contract LTVDecisionEngine {
+	using SafeMath for uint;
+
+	uint public constant PRECISION = 4;
+
 	function evaluateConsent(LTVDecisionEngineTypes.Params params)
 		public view returns (bool signatureValid, bytes32 _id)
 	{
@@ -36,12 +44,49 @@ contract LTVDecisionEngine {
 		);
 	}
 
-	function evaluateDecision(
-		LTVDecisionEngineTypes.Params params
-	)
-	public view returns (bool _success)
+	// Returns true if the creditor-initiated order has not expired, and the LTV is below the max.
+	function evaluateDecision(LTVDecisionEngineTypes.Params params)
+		public view returns (bool _success)
 	{
-		// The CreditorProxyDecisionEngine is naive, and simply returns true.
-		return true;
+		LTVDecisionEngineTypes.Price principalTokenPrice = params.principalPrice;
+		LTVDecisionEngineTypes.Price collateralTokenPrice = params.collateralPrice;
+
+		LTVDecisionEngineTypes.CommitmentValues commitmentValues = params.creditorCommitment.values;
+		OrderLibrary.DebtOrder order = params.order;
+
+		if (isExpired(commitmentValues.expirationTimestamp)) {
+			return false;
+		}
+
+		uint ltv = computeLTV(
+			principalTokenPrice.value,
+			collateralTokenPrice.value,
+			commitmentValues.principalAmount,
+			order.collateralAmount
+		);
+
+		uint maxLTVWithPrecision = maxLTV.mul(10 ** (PRECISION.sub(2)));
+
+		return computedLTV > maxLTVWithPrecision;
+	}
+
+	function computeLTV(
+		uint principalTokenPrice,
+		uint collateralTokenPrice,
+		uint principalAmount,
+		uint collateralAmount
+	)
+		public  constant returns (uint)
+	{
+		uint principalValue = principalTokenPrice.mul(principalAmount).mul(10 ** PRECISION);
+		uint collateralValue = collateralTokenPrice.mul(collateralAmount);
+
+		return principalValue.div(collateralValue);
+	}
+
+	function isExpired(uint expirationTimestampInSec)
+		public view returns (bool expired)
+	{
+		return expirationTimestampInSec < block.timestamp;
 	}
 }
