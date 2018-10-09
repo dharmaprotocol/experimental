cd3edd7db41a0b521f970fd78858bdd62c3b2a08
Graeme <graemelearn@protonmail.com>
2018-10-09 10:03:57 -0700

    feat: add DecisionEngineTypes

---
+++ contracts/src/CreditorDrivenLoans/DecisionEngines/libraries/DecisionEngineTypes.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.4.24;
+
+import "../../../shared/libraries/SignatureTypes.sol";
+
+
+contract DecisionEngineTypes {
+	struct EvaluationParams {
+		address creditor;
+		bytes32[] data;
+		ECDSASignature[] signatures;
+	}
+}
