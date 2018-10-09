9c2e24eacea36a501905eb3394d4d7caa3859705
Graeme <graemelearn@protonmail.com>
2018-10-09 10:35:59 -0700

    refactor: change SignatureTypes to SignaturesLibrary

---
+++ contracts/src/shared/libraries/SignaturesLibrary.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.4.24;
+
+
+contract SignaturesLibrary {
+	struct ECDSASignature {
+		uint8 v;
+		bytes32 r;
+		bytes32 s;
+	}
+
+	function isValidSignature(
+		address signer,
+		bytes32 hash,
+		ECDSASignature signature
+	)
+		public
+		pure
+		returns (bool valid)
+	{
+		bytes32 prefixedHash = keccak256(prefix, hash);
+		return ecrecover(prefixedHash, v, r, s) == signer;
+	}
+}
