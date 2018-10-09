4a2083984e1e7b52034e9e073c75d3b13c5154cf
Kayvon Tehranian <ktehranian@gmail.com>
2018-10-09 15:39:26 -0700

    feat: introduce LTVDecisionEngineInterface

---
+++ contracts/src/CreditorDrivenLoans/interfaces/LTVDecisionEngineInterface.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.25;
+
+import "../DecisionEngines/libraries/LTVDecisionEngineTypes.sol";
+
+contract LTVDecisionEngineInterface {
+
+	function evaluateConsent(LTVDecisionEngineTypes.Params params)
+		public view returns (bool signatureValid, bytes32 _id);
+
+	function evaluateDecision(LTVDecisionEngineTypes.Params params)
+		public view returns (bool _success);
+}
