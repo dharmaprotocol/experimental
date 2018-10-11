ad749c978f67f72b7039fbb0d86b2962fa03327e
Kayvon Tehranian <ktehranian@gmail.com>
2018-10-11 16:37:09 -0700

    fix: import events module

--- contracts/src/CreditorDrivenLoans/interfaces/CreditorProxyCoreInterface.sol
+++ contracts/src/CreditorDrivenLoans/interfaces/CreditorProxyCoreInterface.sol
@@ -3,6 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "../../shared/libraries/OrderLibrary.sol";
 import "../libraries/CreditorProxyErrors.sol";
+import "../libraries/CreditorProxyEvents.sol";
 
 contract CreditorProxyCoreInterface is CreditorProxyErrors, CreditorProxyEvents {
 
