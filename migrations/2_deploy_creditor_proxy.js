a9cc6eeb9eba8af228c48d7f4e54c0b72fe5b908
Graeme <graemelearn@protonmail.com>
2018-10-15 13:43:39 -0700

    build: update dharma-chain

--- migrations/2_deploy_creditor_proxy.js
+++ migrations/2_deploy_creditor_proxy.js
@@ -2,7 +2,7 @@ const NaiveCreditorProxy = artifacts.require("./NaiveCreditorProxy.sol");
 const LTVCreditorProxy = artifacts.require("./LTVCreditorProxy.sol");
 
 module.exports = function(deployer) {
-    const contractRegistry = "0xbeedb1250c757b0687106a137630c06c10876316";
+    const contractRegistry = "0x3a943351745db0cadbedef212e1211af99495af6";
 
     deployer.deploy(
         NaiveCreditorProxy,
