e7ea7af52ec95f78b1bede223c51a655db334a7a
Graeme <graemelearn@protonmail.com>
2018-10-16 13:40:54 -0700

    test: add snapshot revert to LTV tests

--- test/ts/LTVCreditorProxy.spec.ts
+++ test/ts/LTVCreditorProxy.spec.ts
@@ -1,6 +1,7 @@
 // External libraries
 import * as chai from "chai";
 import * as Web3 from "web3";
+import * as ABIDecoder from "abi-decoder";
 // Types
 import { DebtOrderFixtures } from "./fixtures/DebtOrders";
 import { CommitmentValues, CreditorCommitment, LTVParams, Price } from "../../types/LTVTypes";
@@ -8,11 +9,13 @@ import { DebtOrder } from "../../types/DebtOrder";
 import { LTVFixtures } from "./fixtures/LTVFixtures";
 
 import * as addressBook from "dharma-address-book";
+import SnapshotManager from "./helpers/SnapshotManager";
 
 // Artifacts
 const LTVCreditorProxy = artifacts.require("./LTVCreditorProxy.sol");
 const TokenRegistry = artifacts.require("./TokenRegistry.sol");
 const DummyToken = artifacts.require("./DummyToken.sol");
+const DebtKernel = artifacts.require("./DebtKernelInterface.sol");
 
 const addresses = addressBook.latest.development;
 
@@ -23,6 +26,8 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 const proxy = new web3.eth.Contract(LTVCreditorProxy.abi, LTVCreditorProxy.address);
 const registry = new web3.eth.Contract(TokenRegistry.abi, addresses.TokenRegistry);
+
+const snapshotManager = new SnapshotManager(web3);
 
 let debtOrderFixtures: DebtOrderFixtures;
 let lTVFixtures: LTVFixtures;
@@ -38,156 +43,174 @@ let creditor: string;
 let debtor: string;
 
 contract("LTVCreditorProxy", (accounts) => {
-    before(async () => {
-        // To keep things simple, they're just the same for now.
-        creditor = accounts[0];
-        debtor = accounts[0];
-
-        await setupBalancesAndAllowances();
-
-        const tokens = {
-            principalAddress: principalTokenAddress,
-            collateralAddress: collateralTokenAddress,
-        };
-
-        const participants = {
-            creditor,
-            debtor,
-        };
-
-        const contracts = {
-            debtKernelAddress: addresses.DebtKernel,
-            repaymentRouterAddress: addresses.RepaymentRouter,
-            termsContractAddress: addresses.CollateralizedSimpleInterestTermsContract,
-        };
-
-        debtOrderFixtures = new DebtOrderFixtures(web3, accounts, tokens, participants, contracts);
-        lTVFixtures = new LTVFixtures(web3, accounts, tokens, participants, contracts);
-    });
-
-    const setupBalancesAndAllowances = async (): Promise<void> => {
-        principalTokenAddress = await registry.methods.getTokenAddressByIndex(0).call();
-        collateralTokenAddress = await registry.methods.getTokenAddressByIndex(1).call();
-
-        principalToken = new web3.eth.Contract(DummyToken.abi, principalTokenAddress);
-        collateralToken = new web3.eth.Contract(DummyToken.abi, collateralTokenAddress);
-
-        await principalToken.methods.approve(
-            addresses.TokenTransferProxy,
-            100,
-        ).send({ from: accounts[0] });
-
-        await collateralToken.methods.approve(
-            addresses.TokenTransferProxy,
-            100,
-        ).send({ from: accounts[0] });
-    };
-
-    describe("#hashOrder", () => {
-        describe("when given commitment values and a debt order", () => {
-            it("returns the expected bytes32 hash", async () => {
-                const params = await lTVFixtures.unsignedParams();
-                const order = await debtOrderFixtures.unsignedOrder();
-                const commitmentValues = params.creditorCommitment.values;
-
-                const expected = await lTVFixtures.commitmentHash(commitmentValues, order);
-
-                const result = await proxy.methods.hashOrder(commitmentValues, order).call();
-
-                expect(result).to.eq(expected);
-            });
-        });
-    });
-
-    describe("when given params that are signed by the creditor but not the price feed operator", () => {
-        // STUB.
-    });
-
-    describe("when given params that are signed by the creditor and the price feed operator", () => {
-        let order: DebtOrder;
-        let commitmentHash: string;
-        let params: LTVParams;
-
+    describe.only("implementation", () => {
         before(async () => {
-            params = await lTVFixtures.signedParams();
-            order = params.order;
+            // To keep things simple, they're just the same for now.
+            creditor = accounts[0];
+            debtor = accounts[0];
 
-            commitmentHash = await proxy.methods.hashOrder(params.creditorCommitment.values, order).call();
+            await setupBalancesAndAllowances();
+
+            const tokens = {
+                principalAddress: principalTokenAddress,
+                collateralAddress: collateralTokenAddress,
+            };
+
+            const participants = {
+                creditor,
+                debtor,
+            };
+
+            const contracts = {
+                debtKernelAddress: addresses.DebtKernel,
+                repaymentRouterAddress: addresses.RepaymentRouter,
+                termsContractAddress: addresses.CollateralizedSimpleInterestTermsContract,
+            };
+
+            debtOrderFixtures = new DebtOrderFixtures(web3, accounts, tokens, participants, contracts);
+            lTVFixtures = new LTVFixtures(web3, accounts, tokens, participants, contracts);
         });
 
-        it("returns a transaction receipt", async () => {
-            const txReceipt = await proxy.methods.fillDebtOffer(params).send({
-                from: params.creditor,
-                gas: 6712390
+        const setupBalancesAndAllowances = async (): Promise<void> => {
+            principalTokenAddress = await registry.methods.getTokenAddressByIndex(0).call();
+            collateralTokenAddress = await registry.methods.getTokenAddressByIndex(1).call();
+
+            principalToken = new web3.eth.Contract(DummyToken.abi, principalTokenAddress);
+            collateralToken = new web3.eth.Contract(DummyToken.abi, collateralTokenAddress);
+
+            await principalToken.methods.approve(
+                addresses.TokenTransferProxy,
+                100,
+            ).send({ from: accounts[0] });
+
+            await collateralToken.methods.approve(
+                addresses.TokenTransferProxy,
+                100,
+            ).send({ from: accounts[0] });
+        };
+
+        describe("#hashOrder", () => {
+            describe("when given commitment values and a debt order", () => {
+                it("returns the expected bytes32 hash", async () => {
+                    const params = await lTVFixtures.unsignedParams();
+                    const order = await debtOrderFixtures.unsignedOrder();
+                    const commitmentValues = params.creditorCommitment.values;
+
+                    const expected = await lTVFixtures.commitmentHash(commitmentValues, order);
+
+                    const result = await proxy.methods.hashOrder(commitmentValues, order).call();
+
+                    expect(result).to.eq(expected);
+                });
+            });
+        });
+
+        describe("when given params that are signed by the creditor but not the price feed operator", () => {
+            // STUB.
+        });
+
+        describe("#fillDebtOffer", () => {
+            let snapshotId: number;
+
+            before(async () => {
+                ABIDecoder.addABI(DebtKernel.abi);
+
+                snapshotId = await snapshotManager.saveTestSnapshot();
             });
 
-            const txHash = txReceipt.transactionHash;
+            after(async () => {
+                ABIDecoder.removeABI(DebtKernel.abi);
 
-            // The transaction receipt is valid if it has a string transaction hash.
-            expect(txHash).to.be.a("string");
-        });
-
-        it("adds a mapping in the debtOfferFilled field", async () => {
-            const result = await proxy.methods.debtOfferFilled(commitmentHash).call();
-
-            expect(result).to.eq(true);
-        });
-    });
-
-    describe("when the commitment values were not signed", () => {
-        let unsignedOrder: DebtOrder;
-        let commitmentHash: string;
-
-        before(async () => {
-            const params = await lTVFixtures.unsignedParams();
-
-            unsignedOrder = params.order;
-
-            commitmentHash = await proxy.methods.hashOrder(params.creditorCommitment.values, unsignedOrder).call();
-        });
-
-        it("returns a transaction receipt", async () => {
-            const values: CommitmentValues = { maxLTV: 100 };
-
-            const creditorCommitment: CreditorCommitment = {
-                values,
-                signature: debtOrderFixtures.blankSignature
-            };
-
-            const principalPrice: Price = {
-                value: 0,
-                tokenAddress: principalTokenAddress,
-                timestamp: 0,
-                signature: debtOrderFixtures.blankSignature
-            };
-
-            const collateralPrice: Price = {
-                value: 0,
-                tokenAddress: collateralTokenAddress,
-                timestamp: 0,
-                signature: debtOrderFixtures.blankSignature
-            };
-
-            const params: LTVParams = {
-                creditorCommitment,
-                creditor: accounts[0],
-                priceFeedOperator: accounts[1],
-                principalPrice,
-                collateralPrice,
-                order: unsignedOrder
-            };
-
-            const transactionReceipt = await proxy.methods.fillDebtOffer(params).send({
-                from: unsignedOrder.creditor
+                await snapshotManager.revertToSnapshot(snapshotId);
             });
 
-            expect(transactionReceipt.transactionHash).to.be.a("string");
-        });
+            describe("when given unsigned parameters", () => {
+                let unsignedOrder: DebtOrder;
+                let commitmentHash: string;
 
-        it("does not add a mapping in the debtOfferFilled field", async () => {
-            const result = await proxy.methods.debtOfferFilled(commitmentHash).call();
+                before(async () => {
+                    const params = await lTVFixtures.unsignedParams();
 
-            expect(result).to.eq(false);
+                    unsignedOrder = params.order;
+
+                    commitmentHash = await proxy.methods.hashOrder(params.creditorCommitment.values, unsignedOrder).call();
+                });
+
+                it("returns a transaction receipt", async () => {
+                    const values: CommitmentValues = { maxLTV: 100 };
+
+                    const creditorCommitment: CreditorCommitment = {
+                        values,
+                        signature: debtOrderFixtures.blankSignature
+                    };
+
+                    const principalPrice: Price = {
+                        value: 0,
+                        tokenAddress: principalTokenAddress,
+                        timestamp: 0,
+                        signature: debtOrderFixtures.blankSignature
+                    };
+
+                    const collateralPrice: Price = {
+                        value: 0,
+                        tokenAddress: collateralTokenAddress,
+                        timestamp: 0,
+                        signature: debtOrderFixtures.blankSignature
+                    };
+
+                    const params: LTVParams = {
+                        creditorCommitment,
+                        creditor: accounts[0],
+                        priceFeedOperator: accounts[1],
+                        principalPrice,
+                        collateralPrice,
+                        order: unsignedOrder
+                    };
+
+                    const transactionReceipt = await proxy.methods.fillDebtOffer(params).send({
+                        from: unsignedOrder.creditor
+                    });
+
+                    expect(transactionReceipt.transactionHash).to.be.a("string");
+                });
+
+                it("does not add a mapping in the debtOfferFilled field", async () => {
+                    const result = await proxy.methods.debtOfferFilled(commitmentHash).call();
+
+                    expect(result).to.eq(false);
+                });
+            });
+
+            describe("when given params valid signed params", () => {
+                let order: DebtOrder;
+                let commitmentHash: string;
+                let params: LTVParams;
+
+                before(async () => {
+                    params = await lTVFixtures.signedParams();
+                    order = params.order;
+
+                    commitmentHash = await proxy.methods.hashOrder(params.creditorCommitment.values, order).call();
+                });
+
+                it("returns a transaction receipt", async () => {
+                    const txReceipt = await proxy.methods.fillDebtOffer(params).send({
+                        from: params.creditor,
+                        gas: 6712390
+                    });
+
+                    const txHash = txReceipt.transactionHash;
+
+                    // The transaction receipt is valid if it has a string transaction hash.
+                    expect(txHash).to.be.a("string");
+                });
+
+                it("adds a mapping in the debtOfferFilled field", async () => {
+                    const result = await proxy.methods.debtOfferFilled(commitmentHash).call();
+
+                    expect(result).to.eq(true);
+                });
+            });
         });
     });
 });
