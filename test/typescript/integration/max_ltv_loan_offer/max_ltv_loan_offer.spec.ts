1a44c4e7881a6d5e08a8d35e15d9da112a16d08b
Chris Min <chris@dharma.io>
2018-10-15 19:14:41 -0700

    test: signAsCreditor on MaxLTVLoanOffer

--- test/typescript/integration/max_ltv_loan_offer/max_ltv_loan_offer.spec.ts
+++ test/typescript/integration/max_ltv_loan_offer/max_ltv_loan_offer.spec.ts
@@ -4,7 +4,7 @@ import { VALID_MAX_LTV_LOAN_ORDER_PARAMS } from "./scenarios/valid_max_ltv_loan_
 
 import { testConstructor } from "./runners/constructor";
 import { testCreateAndSignAsCreditor } from "./runners/create_and_sign_as_creditor";
-// import { testSignAsCreditor } from "./runners/sign_as_creditor";
+import { testSignAsCreditor } from "./runners/sign_as_creditor";
 // import { testSignAsDebtor } from "./runners/sign_as_debtor";
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
@@ -18,10 +18,10 @@ describe("Max LTV Loan Offer (Integration)", () => {
         await testCreateAndSignAsCreditor(web3, VALID_MAX_LTV_LOAN_ORDER_PARAMS);
     });
 
-    // describe("signAsCreditor", async () => {
-    //     await testSignAsCreditor(dharma, VALID_MAX_LTV_LOAN_ORDER_PARAMS);
-    // });
-    //
+    describe("signAsCreditor", async () => {
+        await testSignAsCreditor(web3, VALID_MAX_LTV_LOAN_ORDER_PARAMS);
+    });
+
     // describe("signAsDebtor", async () => {
     //     await testSignAsDebtor(dharma, VALID_MAX_LTV_LOAN_ORDER_PARAMS);
     // });
