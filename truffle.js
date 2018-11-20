99f0c485b0181d82b677fb9b09dd8fe3842f1dc3
Chris Min <chris@dharma.io>
2018-11-20 14:15:55 -0800

    build: update test directory to "build/js"

--- truffle.js
+++ truffle.js
@@ -10,21 +10,25 @@ module.exports = {
             gasPrice: 1,
         },
         kovan: {
-            provider: () => new HDWalletProvider(
-                process.env.WALLET_KEY, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY
-            ),
+            provider: () =>
+                new HDWalletProvider(
+                    process.env.WALLET_KEY,
+                    "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY
+                ),
             network_id: "1",
             gas: 4000000,
             gasPrice: 22000000000, // 15 GWei, as per https://ethgasstation.info/
         },
         live: {
-            provider: () => new HDWalletProvider(
-                process.env.WALLET_KEY, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY
-            ),
+            provider: () =>
+                new HDWalletProvider(
+                    process.env.WALLET_KEY,
+                    "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY
+                ),
             network_id: "1",
             gas: 4000000,
             gasPrice: 22000000000, // 15 GWei, as per https://ethgasstation.info/
         },
     },
-    test_directory: "test/js",
+    test_directory: "build/js",
 };
